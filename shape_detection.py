223c9700ae022bdbb6a4c03a4b7e4f3e68d00736
Paroma <varma@ubuntu.(none)>
2013-09-07 00:10:11 -0700

    color detection added

--- shape_detection.py
+++ shape_detection.py
@@ -1,4 +1,13 @@
-from SimpleCV import Image, Color, Features
+from SimpleCV import Image, Color, Features, cv, cv2, np
+
+def find_color(x,y):
+    corner = markupImage.crop(x-2, y-2, 4, 4)
+    return corner.meanColor()
+
+
+
+
+
 
 markupImage = Image("/home/varma/Desktop/markup/markup.jpg")
 blobs = markupImage.findBlobs()
@@ -10,9 +19,14 @@ for b in blobs:
     y = info[1]
     w = info[2]
     h = info[3]
+    c = find_color(x,y)
     markupImage.drawRectangle(x,y,w,h)
+    #c is the rgb tuple of the border of blob b  
+
+
 while True:
     markupImage.show()
+
 
 
 
