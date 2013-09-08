ccecaea1e4719668723ced3697cb99ec041bd61b
Vaishaal Shankar <vaishaal@gmail.com>
2013-09-08 02:33:49 -0700

    color detection

--- markup.py
+++ markup.py
@@ -6,8 +6,10 @@ from recordtype import recordtype
 import numpy
 import os
 DEBUG = 1
-Rectangle =  recordtype('rectangle', ['x', 'y', 'w', 'h', 'color', 'shape', 'parent'])
-WebRectangle  = recordtype('webrectangle', ['x', 'y', 'w', 'h', 'shape', 'children', 'parent', 'box_type']) 
+GRIDX = 56
+GRIDY = 36
+Rectangle =  recordtype('rectangle', ['x', 'y', 'w', 'h', 'color'])
+WebRectangle  = recordtype('webrectangle', ['x', 'y', 'w', 'h', 'color']) 
 colors = [Color.YELLOW, Color.HOTPINK, Color.CYAN, Color.RED, Color.BLUE, Color.GREEN]
 Grid = recordtype('grid', ['x','y', 'origin', 'end'])
 Point = recordtype('point', ['x','y'])
@@ -24,9 +26,23 @@ def find_shapes(img, i):
         w = info[2]
         h = info[3]
         #markupImage.drawRectangle(x, y, w, h)
-        r = Rectangle(x, y, w, h, "black", 0, None)
-        r.color = analyze_color(r, markupImage)
-	rectangles.append(r)
+        r = Rectangle(x, y, w, h, 0)
+        rectangles.append(r) 
+
+    bwImage = img.colorDistance((255, 0, 0)).invert().binarize(80)
+    blobs = bwImage.findBlobs()
+    for b in blobs:
+        info = b.boundingBox()
+        x = info[0]
+        y = info[1]
+        w = info[2]
+        h = info[3]
+        #markupImage.drawRectangle(x, y, w, h)
+        r = Rectangle(x, y, w, h, 1)
+        rectangles.append(r) 
+
+    max_rectangle = max(rectangles, key=lambda rect: rect.w * rect.h)
+    markupImage.drawRectangle(max_rectangle.x, max_rectangle.y, max_rectangle.w, max_rectangle.h, Color.ORANGE)  
     return rectangles, img
  
 def grid_transform(info):
@@ -34,64 +50,34 @@ def grid_transform(info):
     max_rectangle = max(rects, key=lambda rect: rect.w * rect.h)
     rects.remove(max_rectangle) 
     rects.sort(key=lambda rect: -1 * rect.w * rect.h)  
-    find_rects_metadata(rects, img)
     origin = Point(max_rectangle.x, max_rectangle.y)
     end = Point(max_rectangle.x + max_rectangle.w, max_rectangle.y + max_rectangle.h)
-    gridx = int(math.ceil(max_rectangle.w/14))
-    gridy = int(math.ceil(max_rectangle.h/9))
+    gridx = int(math.ceil(max_rectangle.w/GRIDX))
+    gridy = int(math.ceil(max_rectangle.h/GRIDY))
     grid = Grid(gridx, gridy, origin, end)  
     web_rects = [] 
     for rect in rects:
         web_rects.append(transform_rect(rect, grid))
-    find_webrects_metadata(rects, web_rects) 
+    
+    draw_grid(img, grid)
+
+    web_rects = list(filter(lambda rect: rect.w > 6 and rect.h > 6, web_rects))
     draw_web_rects(img, web_rects, grid) 
-    return list(filter(lambda rect: rect.w != 0 and rect.h != 0, web_rects)), img
-
-def find_rects_metadata(rects, img):
-    for i, rect in enumerate(rects):
-        parent = None 
-        for j in range(i, -1, -1):
-            if inside(rect, rects[j]):  
-                rect.parent = rects[j]
-                break     
-        rect.shape = get_type(rect, img)
-
-def find_webrects_metadata(rects, webrects):
-    for i, webrect in enumerate(webrects): 
-        webrect.box_type = imagetonum(rects[i].color)
-	if (rects[i].parent):
-            index = rects.index(rects[i].parent)
-            parent = webrects[index]
-            webrect.parent=parent 
-    for rect in webrects:
-        if rect.parent:
-            if rect.shape: 
-                rect.parent.box_type = 1
-                rect.box_type = 2
-            rect.parent.children.append(rect) 
+    return web_rects, img
 
 def imagetonum(str):
-    print str
     if str == "red":
-	return 3
+	    return 3
     elif str == "green":
-	return 4
+	    return 4
     elif str == "blue":
-	return 5
+	    return 5
     else:
-	return 0
+	    return 0
 
     
 def get_type(rect, img):
-    cropped =  img.crop(rect.x, rect.y, rect.w, rect.h)
-    corners = cropped.findCorners(minquality=0.7) 
-    print len(corners)
-    if len(corners) == 3: 
-        return 1 
-    else:
-        return 0 
-
-       
+    return 0
  
 #ONLY FOR TESTING AND DRAWING PURPOSES
 def analyze_shapes(info):
@@ -125,9 +111,8 @@ def transform_rect(rect, grid):
      
     upper_corner = transform_point(Point(rect.x, rect.y), grid) 
     lower_corner = transform_point(Point(rect.x + rect.w, rect.y + rect.h), grid) 
-    print lower_corner
     return WebRectangle(upper_corner.x, upper_corner.y, abs(upper_corner.x - lower_corner.x),
-                 abs(upper_corner.y - lower_corner.y), rect.shape , [], None, 0)
+                 abs(upper_corner.y - lower_corner.y), rect.color)
 
     
 def transform_point(point, grid):
@@ -139,16 +124,16 @@ def transform_point(point, grid):
         new_x = up_x
     if new_x < 0:
         new_x = 0
-    if new_x >= 14:
-        new_x = 13
+    if new_x >= GRIDX:
+        new_x = GRIDX - 1
     up_y = int(math.ceil((point.y - grid.origin.y)/grid.y))
     down_y = int((point.y - grid.origin.y)/grid.y)
     if abs(point.y - up_y) >= abs(point.y - down_y): 
         new_y = down_y
     else:
         new_y = up_y
-    if new_y >= 9:
-        new_y = 8
+    if new_y >= GRIDY:
+        new_y = GRIDY - 1
     if new_y < 0:
         new_y = 0
     return Point(new_x, new_y)
@@ -166,7 +151,7 @@ def draw_web_rects(image, web_rects, grid):
         a_y  = rect.y * grid.y  + grid.origin.y
         a_w =  grid.x * rect.w 
         a_h =  grid.y * rect.h
-        image.drawRectangle(a_x, a_y, a_w, a_h, colors[rect.box_type]) 
+        image.drawRectangle(a_x, a_y, a_w, a_h, colors[rect.color]) 
     return image
     
 def analyze_color(rect, markupImage):
@@ -197,7 +182,7 @@ def analyze(img):
     return grid_transform(find_shapes(Image(img), 50))
 
 def get_rows(webrectangles, img):
-    rows = [[] for i in range(9)] 
+    rows = [[] for i in range(GRIDY)] 
     for rect in webrectangles:
         rows[rect.y].append(rect) 
     for row in rows:
